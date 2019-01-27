3eb246050ee987d27129ca065b007839825d5e1c
evankawa <evank@kinetisense.com>
2019-01-27 15:46:15 -0700

    EPilogue

--- MyNeighbourTheVampire/Assets/Scripts/DayCompleteEvent.cs
+++ MyNeighbourTheVampire/Assets/Scripts/DayCompleteEvent.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fungus;
 
 public class DayCompleteEvent : GameEvent
 {
@@ -8,5 +9,7 @@ public class DayCompleteEvent : GameEvent
 	{
 		UIFader fader = CanvasManager.instance.Get<UIFader>(UIPanelID.Fader);
 		yield return fader.EndDay();
+		ConversationManager.Instance.Variables["guests"] = GameManager.Instance._numGuests.ToString();
+		ConversationManager.Instance.Variables["party"] = (GameManager.Instance._numGuests >= 104 ? "success" : "fail");
 	}
 }
---
+++ MyNeighbourTheVampire/Assets/Scripts/GameEndEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEndEvent : GameEvent
+{
+	public override IEnumerator Run()
+	{
+		EndScreen fader = CanvasManager.instance.Get<EndScreen>(UIPanelID.EndScreen);
+		fader.Open();
+		yield return null;
+	}
+}
